b0ac4bf87aeb6cf656716fe9ff8fcb1bd3b465e7
Syed Sajjad Askari <syedsajjad62@gmail.com>
2025-03-02 09:35:17 +0000

    home.py code updated

---
+++ src/ML_XAI_Dashboard/components/Home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from st_aggrid import AgGrid
+
+def write(state):
+    with st.spinner("Loading Home ..."):
+        task_type = st.radio("Please Select the Task Type: ", options=["Regression", "Classification"])
+        state = st.file_uploader('Upload csv file for project', type=["csv", "xlsx"])
+        if state is not None:
+            file_extension = state.name.split('.')[1]
+            if file_extension == "csv":
+                state.df = pd.read_csv(state)
+            else:
+                state.df = pd.read_excel(state)
+            st.header("The First 10 Rows of Data")
+            #st.table(state.df.head(20))
+            AgGrid(state.df.head(10))
+            
+            if state.df is not None:
+                state.task_type=task_type
+                return state.df, state.task_type
+            return state.df,"No Defined"
