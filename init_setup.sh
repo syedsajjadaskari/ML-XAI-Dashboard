2479fc3f361cabe8b46b1d3f79dc97b46201d244
Syed Sajjad Askari <syedsajjad62@gmail.com>
2025-03-02 09:14:31 +0000

    add the initial setup

---
+++ init_setup.sh
@@ -0,0 +1,8 @@
+echo [$(date)]: "START"
+echo [$(date)]: "creating env with python 3.12 version"
+virtualenv venv --python=python3.12
+echo [$(date)] "Activationg the dev envirnmnets"
+. venv/bin/activate
+echo [$(date)]: "installing the  dev requirements"
+pip install -r requirements_dev.txt
+echo [$(date)]: "END"
