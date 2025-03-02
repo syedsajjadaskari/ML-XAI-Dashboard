74795bbda086da9305079ea6a3ee093095fa27e0
Syed Sajjad Askari <syedsajjad62@gmail.com>
2025-03-02 10:35:59 +0000

    add ydata_profiling

---
+++ src/ML_XAI_DASHBOARD/components/DataInfo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
+@st.cache(suppress_st_warning=True)
+def write(state_df):
+    st.header("Loading Data Info ...")
+    with st.spinner("Loading Data Info...."):
+        if state_df is not None:
+            pr=ProfileReport(state_df,explorative=True, minimal=True)
+            st_profile_report(pr)
+        else:
+            st.error("Please Upload Dataset First 🚨")
