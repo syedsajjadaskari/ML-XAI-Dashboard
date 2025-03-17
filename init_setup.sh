2ecc889d2dd2015011a969f4ccf87af845445b79
Syed Sajjad Askari <syedsajjad62@gmail.com>
2025-03-17 06:21:21 +0000

    update bash file

--- init_setup.sh
+++ init_setup.sh
@@ -1,6 +1,6 @@
 echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.12 version"
-virtualenv venv --python=python3.12
+virtualenv venv --python=python3.11
 echo [$(date)] "Activationg the dev envirnmnets"
 . venv/bin/activate
 echo [$(date)]: "installing the  dev requirements"
